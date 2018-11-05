ab7f14da1a4e7869433b26a88d420790316bde5c
Lena <happybeelena@gmail.com>
2018-11-06 01:15:13 +0300

    kontrosha

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+     class Test
+    {
+
+        public int elem;
+        public static bool operator * (Test a, Test b)
+        {
+            if(a.elem == b.elem)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test A = new Test();
+            A.elem = 5;
+            Test B = new Test();
+            B.elem = 5;
+            if (A * B)
+            {
+                Console.WriteLine(" true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+
+
+}
